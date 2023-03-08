d53342fd1457e886e6971a5946220c4f3866905d
FCC\patidarn <patidarn@fcc.ca>
2023-03-08 11:46:51 -0600

    Update MyFirstRelease.java

--- src/main/java/com/release/testing/MyFirstRelease.java
+++ src/main/java/com/release/testing/MyFirstRelease.java
@@ -4,6 +4,6 @@ public class MyFirstRelease
 {
     public static void main( String[] args )
     {
-        System.out.println( "My First Release !" );
+        System.out.println( "My First Release!" );
     }
 }
